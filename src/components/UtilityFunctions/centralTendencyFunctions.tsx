c5318c1af1b7a913b02aa3d29f118a78ce46b226
MUNEERWANI <muneerwani78@gmail.com>
2023-09-25 21:43:08 +0530

    Complete

--- src/components/UtilityFunctions/centralTendencyFunctions.tsx
+++ src/components/UtilityFunctions/centralTendencyFunctions.tsx
@@ -1,11 +1,24 @@
 export const calculateMean = (data: number[]) => {
-    const sum = data.reduce((acc, value) => acc + value, 0);
-    return sum / data.length;
+    const filteredData = data.filter(value => typeof value === 'number' && !isNaN(value));
+  
+    if (filteredData.length === 0) {
+      return 0;
+    }
+  
+    const sum = filteredData.reduce((acc, value) => acc + value, 0);
+    return sum / filteredData.length;
   };
   
+  
   export const calculateMedian = (data: number[]) => {
-    const sortedData = data.sort((a, b) => a - b);
+    const filteredData = data.filter(value => !isNaN(value));
+  
+    const sortedData = filteredData.sort((a, b) => a - b);
     const middle = Math.floor(sortedData.length / 2);
+  
+    if (sortedData.length === 0) {
+      return 0;
+    }
   
     if (sortedData.length % 2 === 0) {
       return (sortedData[middle - 1] + sortedData[middle]) / 2;
@@ -15,9 +28,11 @@ export const calculateMean = (data: number[]) => {
   };
   
   export const calculateMode = (data: number[]) => {
+    const filteredData = data.filter(value => !isNaN(value));
+  
     const countMap = new Map<number, number>();
   
-    for (const value of data) {
+    for (const value of filteredData) {
       countMap.set(value, (countMap.get(value) || 0) + 1);
     }
   
@@ -31,5 +46,7 @@ export const calculateMean = (data: number[]) => {
       }
     }
   
-    return mode !== undefined ? mode : 0; 
+    return mode !== undefined ? mode : 0;
   };
+    
+  
